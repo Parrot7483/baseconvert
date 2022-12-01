82801dc44e9b19f33909fc19e0ed5456a5775350
Tristan Schwieren <tristan.schwieren@tum.de>
2022-12-01 11:58:11 +0100

    tests

---
+++ tests.cpp
@@ -0,0 +1,12 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+#include "BaseConvert.h"
+
+TEST_CASE("testing the baseconvert library") {
+    baseconvert::BaseConvert t1 = baseconvert::BaseConvert("0123456789", "0123456789ABCDEF");
+    std::string input_string1 {"255"};
+    std::vector<unsigned char> input1(input_string1.begin(), input_string1.end());
+    std::string result1 = t1.encode(input1);
+    CHECK(result1 == "FF");
+    CHECK(t1.decode(result1) == input1);
+}
