b6d5c536435a99e80ad91b43d72fba823bd434a9
Tristan Schwieren <tristan.schwieren@tum.de>
2022-12-01 12:40:06 +0100

    added missing doctest dependency

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [ pkgs.gcc pkgs.cmake pkgs.gmp pkgs.fmt pkgs.bash ];
+    nativeBuildInputs = [ pkgs.gcc pkgs.cmake pkgs.gmp pkgs.fmt pkgs.doctest pkgs.bash ];
 }
